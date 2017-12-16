cd8afa59ac8dfb2889b97a8423fe8ea2377bc742
xingziyang <ziyanghenan@126.com>
2017-12-16 16:36:44 +0800

    update config refresh

--- src/main/java/com/roncoo/eshop/web/controller/GreetingController.java
+++ src/main/java/com/roncoo/eshop/web/controller/GreetingController.java
@@ -2,13 +2,17 @@ package com.roncoo.eshop.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.roncoo.eshop.service.SayHelloService;
 
+@EnableAutoConfiguration
 @RestController
+@RefreshScope
 public class GreetingController {
 
 //	@Autowired
